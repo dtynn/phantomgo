47039d1c2ad9b7736947b2914da27f6b00963d6c
dtynn <dtynn@163.com>
2016-10-12 21:49:39 +0800

    use field tag

--- core/handlers/on_error.go
+++ core/handlers/on_error.go
@@ -4,26 +4,12 @@ import (
 	"github.com/gopherjs/gopherjs/js"
 )
 
-func NewErrorStack(obj *js.Object) *ErrorStack {
-	return &ErrorStack{
-		obj: obj,
-	}
-}
-
 type ErrorStack struct {
-	obj *js.Object
-}
+	*js.Object
 
-func (this *ErrorStack) File() string {
-	return this.obj.Get("file").String()
-}
-
-func (this *ErrorStack) Line() int {
-	return this.obj.Get("line").Int()
-}
-
-func (this *ErrorStack) Function() string {
-	return this.obj.Get("function").String()
+	File     string `js:"file"`
+	Line     int    `js:"line"`
+	Function string `js:"function"`
 }
 
 type OnErrorHandler func(msg string, stack []*ErrorStack)
