8664f54cabe3ec3a20ede362498315f4a125ac65
dtynn <dtynn@163.com>
2016-10-25 20:30:35 +0800

    fix http headers

--- core/webpage/resource.go
+++ core/webpage/resource.go
@@ -1,6 +1,7 @@
 package webpage
 
 import (
+	"strings"
 	"time"
 
 	"github.com/dtynn/phantomgo/core/utils"
@@ -11,6 +12,25 @@ const (
 	ResourceResponseStageStart = "start"
 	ResourceResponseStageEnd   = "end"
 )
+
+type Headers []ResponseHeader
+
+func (this Headers) Get(name string) (string, bool) {
+	for _, one := range this {
+		if strings.ToUpper(strings.TrimSpace(one.Name)) == strings.ToUpper(strings.TrimSpace(name)) {
+			return one.Value, true
+		}
+	}
+
+	return "", false
+}
+
+type ResponseHeader struct {
+	obj *js.Object
+
+	Name  string `js:"name"`
+	Value string `js:"value"`
+}
 
 type ResourceError struct {
 	obj *js.Object
@@ -24,15 +44,15 @@ type ResourceError struct {
 type ResourceResponse struct {
 	obj *js.Object
 
-	Id          int               `js:"id"`
-	Url         string            `js:"url"`
-	Headers     map[string]string `js:"headers"`
-	BodySize    int               `js:"bodySize"`
-	ContentType string            `js:"contentType"`
-	RedirectUrl string            `js:"redirectURL"`
-	Stage       string            `js:"stage"`
-	Status      int               `js:"status"`
-	StatusText  string            `js:"statusText"`
+	Id          int     `js:"id"`
+	Url         string  `js:"url"`
+	Headers     Headers `js:"headers"`
+	BodySize    int     `js:"bodySize"`
+	ContentType string  `js:"contentType"`
+	RedirectUrl string  `js:"redirectURL"`
+	Stage       string  `js:"stage"`
+	Status      int     `js:"status"`
+	StatusText  string  `js:"statusText"`
 }
 
 func (this *ResourceResponse) Time() *time.Time {
@@ -42,10 +62,10 @@ func (this *ResourceResponse) Time() *time.Time {
 type ResourceRequest struct {
 	obj *js.Object
 
-	Id      int               `js:"id"`
-	Method  string            `js:"method"`
-	Url     string            `js:"url"`
-	Headers map[string]string `js:"headers"`
+	Id      int     `js:"id"`
+	Method  string  `js:"method"`
+	Url     string  `js:"url"`
+	Headers Headers `js:"headers"`
 }
 
 func (this *ResourceRequest) Time() *time.Time {
@@ -71,12 +91,12 @@ func (this *ResourceNetworkRequest) SetHeader(key, val string) {
 type TimeoutRequest struct {
 	obj *js.Object
 
-	Id          int               `js:"id"`
-	Method      string            `js:"method"`
-	Url         string            `js:"url"`
-	Headers     map[string]string `js:"headers"`
-	ErrorCode   int               `js:"errorCode"`
-	ErrorString string            `js:"errorString"`
+	Id          int     `js:"id"`
+	Method      string  `js:"method"`
+	Url         string  `js:"url"`
+	Headers     Headers `js:"headers"`
+	ErrorCode   int     `js:"errorCode"`
+	ErrorString string  `js:"errorString"`
 }
 
 func (this *TimeoutRequest) Time() *time.Time {
